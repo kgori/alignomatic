cea6579be21c79b9ae0a2116c6874ee9b41974c2
Kevin Gori <kcg25@cam.ac.uk>
2025-04-14 13:31:34 +0000

    Set default logging level to 'INFO'

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use read_pair_io::{MappedReadPair, ReadPairIterator};
 use utils::{bam_to_fastq, create_bgzf_fastq_writer};
 
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let opts: cli::ProgramOptions = cli::get_program_options()?;
 
